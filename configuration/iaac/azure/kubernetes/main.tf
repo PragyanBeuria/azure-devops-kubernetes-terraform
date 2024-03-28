9b6d158d1022f92b841385708d0c3655aab2ef02
Pragyan <87907959+PragyanBeuria@users.noreply.github.com>
2024-03-28 22:02:07 +0530

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
 
     ssh_key {
       key_data = file("${path.module}/${var.ssh_public_key}")
-      key_data = file(var.ssh_public_key)
+      #key_data = file(var.ssh_public_key)
     }
   }
 
