82db9992e2a485261cae27c3bebda7af2dd03285
Pragyan <87907959+PragyanBeuria@users.noreply.github.com>
2024-03-31 18:08:00 +0530

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
     admin_username = "ubuntu"
 
     ssh_key {
-      key_data = file(var.ssh_public_key)
+      key_data = file("./myazure_rsa.hub")
     }
   }
 
